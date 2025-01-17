dc4d64c863ec17a534c66c58679116cb037d1944
ptdat011002 <phamtridat01gmail.com>
2025-01-17 17:08:22 +0700

    fix bug

--- src/app/api/room-reviews/[id]/route.ts
+++ src/app/api/room-reviews/[id]/route.ts
@@ -3,19 +3,19 @@ import { NextResponse } from 'next/server';
 
 export async function GET(
   req: Request,
-  context: { params: { id: string } }
+  { params }: { params: { id: string } }
 ) {
-  const roomId = context.params.id;
+  const roomId = params.id;
 
   try {
     const roomReviews = await getRoomReviews(roomId);
 
     return NextResponse.json(roomReviews, {
       status: 200,
-      statusText: 'Successful',
+      statusText: 'Succesful',
     });
   } catch (error) {
-    console.error('Getting Review Failed', error);
+    console.log('Getting Review Failed', error);
     return new NextResponse('Unable to fetch', { status: 400 });
   }
 }
